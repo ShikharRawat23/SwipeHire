56c8f1832faf3d54335bf004b79a2f9b57c361f9
ShikharRawat23 <shikharrawat23@gmail.com>
2026-02-03 15:22:52 +0530

    Fix custom user model login

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,10 +1,9 @@
 from django.contrib.auth import get_user_model
-
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+
 User = get_user_model()
 
 
@@ -14,25 +13,19 @@ class RegisterView(APIView):
     def post(self, request):
         username = request.data.get("username")
         password = request.data.get("password")
-        role = request.data.get("role", "jobseeker")
 
         if not username or not password:
-            return Response({"error": "Username and password required"}, status=400)
+            return Response({"error": "Missing fields"}, status=400)
 
         if User.objects.filter(username=username).exists():
             return Response({"error": "User already exists"}, status=400)
 
-        user = User.objects.create_user(
+        User.objects.create_user(
             username=username,
             password=password
         )
 
-        return Response({
-            "message": "User registered successfully",
-            "username": user.username,
-            "role": role
-        })
-
+        return Response({"message": "User registered successfully"})
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -42,18 +35,14 @@ class LoginView(APIView):
         username = request.data.get("username")
         password = request.data.get("password")
 
-        if not username or not password:
-            return Response({"error": "Missing fields"}, status=400)
-
-        user = User.objects.filter(username=username).first()
-
-        if not user:
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
             return Response({"error": "Invalid credentials"}, status=400)
 
         if not user.check_password(password):
             return Response({"error": "Invalid credentials"}, status=400)
 
         return Response({
-            "username": user.username,
-            "role": "jobseeker"
+            "username": user.username
         })
